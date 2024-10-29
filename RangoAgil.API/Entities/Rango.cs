250c2555cbaf41376ad85013f0c3f2bbf05f58ea
begsa <begsa@LAPTOP-JCIDAON7>
2024-10-28 21:40:03 -0300

    Adicionar arquivos de projeto.

---
+++ RangoAgil.API/Entities/Rango.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace RangoAgil.API.Entities;
+
+public class Rango
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(200)]
+    public required string Nome { get; set; }
+
+    public ICollection<Ingrediente> Ingredientes { get; set; } = []; // relacionamento n:n (Rango:Ingrediente)
+
+    public Rango()
+    {
+            
+    }
+    [SetsRequiredMembers]
+    public Rango(int id, string nome)
+    {
+        Id = id;
+        Nome = nome;
+    }
+}
